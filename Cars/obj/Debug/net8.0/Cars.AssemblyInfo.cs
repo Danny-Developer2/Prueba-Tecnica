c084b3103b5f9036c15fb2388a12b67510ee9879
Daniel Luevano <daniel_luevanolr@hotmail.com>
2024-11-11 06:06:52 -0600

    Prueba Tecnica

--- Cars/obj/Debug/net8.0/Cars.AssemblyInfo.cs
+++ Cars/obj/Debug/net8.0/Cars.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cars")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52797f439dab28fe8a1e7d02095ad6b6c8834faf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cars")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cars")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
